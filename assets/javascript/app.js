df482279b1001b1d8852dbfe3965450837ec81e1
Rob Canfield <git@livesenjoyable.com>
2019-08-12 16:46:18 -0600

    buttons are being added to HTML with one songle class for all buttons. Results from API are displaying in console. Having trouble getting each button to have its own attribute. Also having a hard time displaying the image.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,5 +2,54 @@ var topics = ['Hannibal Burress', 'Pete Holmes', 'Eric Andre', 'Ryan Hamilton', 
 
 
 for (i = 0; i < topics.length; i++){
-    $("#topics").append("<button>" + topics[i] + "</button>");
+    var item = topics[i]
+    $("#topics").append("<button>" + item + "</button>");
 }
+
+
+
+$("button").attr("data-comedian", "Hannibal Burress");
+
+
+$("button").click(function (){
+    var comedian = $(this).attr("data-comedian");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    comedian + "&api_key=hVU4Q9qva6zSLrNJYez5dI7hZJ6m0jZ6";
+
+    $.ajax({
+
+        url: queryURL,
+        method: "GET"
+
+    }).then(function(response){
+        console.log(response.data);
+
+        var results = response.data;
+
+        for (i = 0; i < results.length; i++){
+            if (results[i].rating !== "r"){
+
+                var gifDiv = $("<div>");
+
+                var rating = results[i].rating
+
+                var p = $("<p>Rating: " + rating + "</p>");
+
+                var image = $("<img>"); 
+
+                image.attr("src", results[i].image)
+
+                gifDiv.append(p);
+                gifDiv.append(image);
+
+                $("#gifs-here").prepend(gifDiv);
+            }
+        }
+    })
+
+
+})
+
+
+
